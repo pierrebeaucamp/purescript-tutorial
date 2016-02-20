88c05eedec6d694f23fa418a3210477ce31f86d6
Pierre Beaucamp <mail@pierrebeaucamp.com>
2016-02-19 22:33:10 -0500

    Updated comments to match Haddock style

--- src/Main.purs
+++ src/Main.purs
@@ -10,15 +10,15 @@ import DOM
 import Prelude
 
 {-|
-    generateBadge is the callback method for the event listener of the button.
-    It will generate a random name to show on our badge.
+  | generateBadge is the callback method for the event listener of the button.
+  | It will generate a random name to show on our badge.
 -}
 generateBadge :: forall eff. DOMEvent -> Eff (dom :: DOM | eff) Unit
 generateBadge event = setBadgeName "Anne Bonney"
 
 {-|
-    main is the entry point of this program. Its sole purpose is to add event
-    listeners to the button and the input field.
+  | main is the entry point of this program. Its sole purpose is to add event
+  | listeners to the button and the input field.
 -}
 main :: forall eff. Eff (dom :: DOM | eff) Unit
 main = do
@@ -26,15 +26,15 @@ main = do
     querySelector "#generateButton" >>= unsafeAddEventListener "click" generateBadge
 
 {-|
-    querySelector is a shorthand function to select an element from the global
-    document using a query.
+  | querySelector is a shorthand function to select an element from the global
+  | document using a query.
 -}
 querySelector :: forall eff. String -> Eff (dom :: DOM | eff) HTMLElement
 querySelector query = unsafeDocument globalWindow >>= unsafeQuerySelector query
 
 {-|
-    setBadgeName sets the name of the Badge and calls 'disableIfEmpty'
-    afterwards.
+  | setBadgeName sets the name of the Badge and calls 'disableIfEmpty'
+  | afterwards.
 -}
 setBadgeName :: forall eff. String -> Eff (dom :: DOM | eff) Unit
 setBadgeName name = do
@@ -44,15 +44,15 @@ setBadgeName name = do
     unsafeTextContent badge >>= disableIfEmpty button
 
 {-|
-    updateBadge is the callback method for the event listener of the input
-    field. It will display the value of the input field on the badge.
+  | updateBadge is the callback method for the event listener of the input
+  | field. It will display the value of the input field on the badge.
 -}
 updateBadge :: forall eff. DOMEvent -> Eff (dom :: DOM | eff) Unit
 updateBadge event = unsafeEventTarget event >>= unsafeValue >>= setBadgeName
 
 {-|
-    disableIfEmpty disables a provided HTMLElement if the *content* string is
-    empty.
+  | disableIfEmpty disables a provided HTMLElement if the *content* string is
+  | empty.
 -}
 disableIfEmpty :: forall eff. HTMLElement -> String -> Eff (dom :: DOM | eff) Unit
 disableIfEmpty button content = if eq content "" then do
